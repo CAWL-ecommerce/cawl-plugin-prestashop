d5f8664665f3ce39a52f12df3cc715ab9d32a1e3
CAWL ecommerce <ecommerce-integrations@cawl.com>
2025-11-04 10:15:17 +0100

    Fix error when retrieving transaction details on order details page
    ISSUE: CS-7520

--- src/Presenter/TransactionPresenter.php
+++ src/Presenter/TransactionPresenter.php
@@ -91,6 +91,11 @@ class TransactionPresenter implements PresenterInterface
                 ))) {
                     try {
                         $payment = $this->merchantClient->payments()->getPayment($paymentDetail->getId());
+                    } catch (\Exception $e) {
+                        $payment = $paymentDetails;
+                    }
+
+                    try {
                         $refunds = $this->merchantClient->payments()->getRefunds($paymentDetail->getId());
                         $captures = $this->merchantClient->payments()->getCaptures($paymentDetail->getId());
                         $paymentSpecificOutput = $this->getPaymentSpecificOutput(
